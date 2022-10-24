ef1357119600590ce93e6b0e6b9cd09225a2431e
acastaneda1200 <acastaneda.1200@gmail.com>
2022-10-24 08:36:29 -0500

    first commit

---
+++ src/modules/daybook/layouts/DayBookLayout.vue
@@ -0,0 +1,23 @@
+<template>
+  <Navbar />
+  <div class="d-flex">
+      <EntryList />
+  </div>
+  <div class="col">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import { defineAsyncComponent } from 'vue'
+export default {
+  components: {
+    Navbar: defineAsyncComponent(() => import('../components/Navbar.vue')),
+    EntryList: defineAsyncComponent(() => import('../components/EntryList.vue'))
+  }
+}
+</script>
+
+<style>
+
+</style>
